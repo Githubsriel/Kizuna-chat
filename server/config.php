711a4555f048be03fd75980641fb1a6f1bac3d65
Asriel <gh@veggofy.de>
2025-06-30 14:58:00 +0200

    Create config.php

---
+++ server/config.php
@@ -0,0 +1,22 @@
+<?php
+// database hostname, you don't usually need to change this
+define('db_host', '-');
+// database username
+define('db_user', '-');
+// database password
+define('db_pass', '-');
+// database name
+define('db_name', '-');
+// database charset, change this only if utf8 is not supported by your language
+define('db_charset', 'utf8');
+// Email activation variables
+// account activation required?
+define('account_activation', true);
+// Change "Your Company Name" and "yourdomain.com", do not remove the < and >
+define('mail_from', 'Kizuna Chat <noreply@kizuna-chat.de>');
+// Link to activation file, update this
+define('activation_link', 'https://kizuna-chat.de/server/activate.php');
+// In config.php or main.php (make sure this file is secure)
+define('ENCRYPTION_KEY', '-');
+
+?>
